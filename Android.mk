d210eedd2b2ae1bb8ba3385b95422947b843d818
Chris Dearman <chris@mips.com>
2011-11-17 18:02:55 -0800

    [MIPS] Use prebuilt clang
    Signed-off-by: Chris Dearman <chris@mips.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ CLANG_ROOT_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
+ifneq ($(TARGET_ARCH),mips)
 subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
   lib/Analysis \
   lib/AST \
@@ -28,3 +29,8 @@ subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
 include $(LOCAL_PATH)/clang.mk
 
 include $(subdirs)
+else
+# MIPS: Copy the prebuilt CLANG from prebuilt/android-mips/llvm/
+$(HOST_OUT_EXECUTABLES)/clang$(HOST_EXECUTABLE_SUFFIX): $(HOST_OUT_EXECUTABLES)/acp$(HOST_EXECUTABLE_SUFFIX)
+	$(ACP) -p prebuilt/android-mips/llvm/clang $(HOST_OUT_EXECUTABLES)/clang$(HOST_EXECUTABLE_SUFFIX)
+endif
